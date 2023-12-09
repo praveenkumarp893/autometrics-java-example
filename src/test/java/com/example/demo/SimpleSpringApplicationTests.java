97922df410f8acb91dcfe8ab75b06fe5a15ae357
praveen <praveen.kprabhakaran@ust.com>
2023-12-09 19:49:38 +0530

    initial commit

---
+++ src/test/java/com/example/demo/SimpleSpringApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+/**
+ * ! Making some tests to this simple Books API
+ */
+
+@SpringBootTest
+class SimpleSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
